3d1ce32669e320c0aff036ffb40b08debf4c4e92
sarahabing <abingsa1@gmail.com>
2023-04-09 19:48:59 -0600

    yes

--- sarah-portfolio/src/App.tsx
+++ sarah-portfolio/src/App.tsx
@@ -1,6 +1,16 @@
-import { Box } from '@mui/material'
+import {
+  AppBar,
+  Avatar,
+  Box,
+  Grid,
+  IconButton,
+  Toolbar,
+  Typography
+} from '@mui/material'
 import './App.css'
-import Background from './Background'
+import sarah from './assets/logos/sarah.jpg'
+import github from './assets/logos/github.png'
+import hackerone from './assets/logos/hackerone.png'
 
 function App() {
   return (
@@ -14,9 +24,43 @@ function App() {
       <div className='light x7'></div>
       <div className='light x8'></div>
       <div className='light x9'></div>
-      <Box component='main' className='App'>
-        main page
-      </Box>
+      <Grid container spacing={8}>
+        <Grid item xs={2}>
+          <Avatar
+            alt='Sarah Dockter'
+            src={sarah}
+            sx={{ width: 50, height: 50 }}
+          />
+        </Grid>
+        <Grid item xs={10}>
+          <Typography variant='h1'>Sarah Dockter</Typography>
+          <span className='emoji'>&#128218; </span>
+          <Typography sx={{verticalAlign: 'super'}}variant='button'>
+            Currently studying for Security+
+          </Typography>
+        </Grid>
+        <AppBar component='nav' sx={{ bgcolor: 'transparent' }}>
+          <Toolbar sx={{ justifyContent: 'flex-end' }}>
+            <Box sx={{ display: { xs: 'none', sm: 'block' } }}>
+              <IconButton href='https://github.com/sarahabing' target='_blank'>
+                <img className='logo' src={github} />
+              </IconButton>
+              <IconButton
+                href='https://hackerone.com/devdockter'
+                target='_blank'
+              >
+                <img className='logo hackerone' src={hackerone} />
+              </IconButton>
+            </Box>
+          </Toolbar>
+        </AppBar>
+      </Grid>
+
+      <Grid container mt={3} mr={10} px={3}>
+        <Grid item xs={12}>
+          <Box component='main'></Box>
+        </Grid>
+      </Grid>
     </>
   )
 }
